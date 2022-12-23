d470e90267f55243768ecee1ec562bce1460497f
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2022-12-23 23:03:41 +0900

    add docs

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/yyamanoi1222/terraform-provider-wordpress/wordpress"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
   err :=providerserver.Serve(context.Background(), wordpress.New, providerserver.ServeOpts{
     Address: "registry.terrafomr.io/yyamanoi1222/wordpress",
